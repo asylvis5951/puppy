4bdff037ea023f2cfc39f45cb58dc403b8b7c84e
asylvis5951 <asylvis@gmail.com>
2023-07-29 16:52:02 -0700

    added BigBark

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/asylvis5951/puppy
 
 go 1.20
+
+require github.com/asylvis5951/dog v0.0.0-20230729234106-581a4d10f2a1
--- puppy.go
+++ puppy.go
@@ -1,9 +1,17 @@
 package puppy
 
+import (
+	"github.com/asylvis5951/dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Barks())
 }
